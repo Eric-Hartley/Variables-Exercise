ad4cc538e537124da6706640b7bba8c922be1f5c
Eric Hartley <spiffydude0@gmail.com>
2024-06-10 19:48:38 -0500

    updated code to reflect exercise reqs

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Eric";
+            int age = 24;
+            char mySymbol = '$';
+            bool isAlive = false;
+            double monthlyIncome = 3600.00;
+            decimal myDebt = 2500.00m;
+
+            Console.WriteLine($"My name is {myName} and I am {age} years old.");
+            Console.WriteLine($"My current debt to income ratio is {mySymbol}{myDebt}/{mySymbol}{monthlyIncome}");
+            Console.WriteLine($"Am I still alive?");
+            if (isAlive)
+            {
+                Console.WriteLine("I am!");
+            }
+            else
+            {
+                Console.WriteLine("...");
+            }
         }
     }
 }
